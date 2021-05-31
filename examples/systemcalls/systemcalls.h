55bd57674e6058c3161a6a43d114bafd577d4dc4
Dan Walkes <danwalkes@trellis-logic.com>
2021-05-31 16:05:01 -0600

    Move systemcalls to assignment 3

--- examples/systemcalls/systemcalls.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdarg.h>
-
-bool do_system(const char *command);
-
-bool do_exec(int count, ...);
-
-bool do_exec_redirect(const char *outputfile, int count, ...);
